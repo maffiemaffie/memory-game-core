3f55b587c37d5986d508010ba5c2a227198ede49
Mafffie Cohen <mrc6113@rit.edu>
2023-11-10 16:05:22 -0500

    hi maffie

--- memoryGameCore/memoryGameCore.cpp
+++ memoryGameCore/memoryGameCore.cpp
@@ -1,5 +1,51 @@
 #include "memoryGameCore.h"
 
+void MemoryGame::start() {
+	state = MemoryGame::State::Starting;
+	changedState.notifyAllObservers(state);
+	compareIndex = 0;
+	pattern = {};
+
+	state = MemoryGame::State::Active;
+	changedState.notifyAllObservers(state);
+	newPattern.notifyAllObservers(getNextPattern());
+}
+
+void MemoryGame::registerObserver(Event event, void (*onEvent)(auto)) {
+	switch (event) {
+	case MemoryGame::Event::onChangedState:
+		changedState.registerObserver(onEvent);
+		break;
+	case MemoryGame::Event::onGameEnded:
+		gameEnded.registerObserver(onEvent);
+		break;
+	case MemoryGame::Event::onNewPattern:
+		newPattern.registerObserver(onEvent);
+		break;
+	default:
+		return false;
+	}
+	return true;
+}
+
+bool MemoryGame::detachObserver(Event event, void (*onEvent)(auto)) {
+	switch (event) {
+	case MemoryGame::Event::onChangedState:
+		changedState.detachObserver(onEvent);
+		break;
+	case MemoryGame::Event::onGameEnded:
+		gameEnded.detachObserver(onEvent);
+		break;
+	case MemoryGame::Event::onNewPattern:
+		newPattern.detachObserver(onEvent);
+		break;
+	default:
+		return false;
+	}
+
+	return true;
+}
+
 int* MemoryGame::getNextPattern() {
 	int nextValue = rand() * 4 / RAND_MAX;
 	pattern.push_back(nextValue);
@@ -8,4 +54,32 @@ int* MemoryGame::getNextPattern() {
 	for (int i = 0; i < pattern.size(); i++) {
 		patternArray[i] = pattern.at(i);
 	}
+
+	return patternArray;
+}
+
+bool MemoryGame::input(int value) {
+	if (gameState != MemoryGame::State::Active) return false; // not ready for input
+	if (input < 0 || input > 3) return false; // out of range
+
+	processNextInput(value);
+	return true;
+}
+
+void MemoryGame::processNextInput(int input) {
+	// check that pattern is correct
+	if (pattern.at(compareIndex) == input) compareIndex++;
+	else return end();
+
+	// continue game
+	if (compareIndex == pattern.size()) {
+		compareIndex = 0;
+		newPattern.notifyAllObservers(getNextPattern());
+	}
+}
+
+void MemoryGame::end() {
+	state = MemoryGame::State::Ending;
+	changedState.notifyAllObservers(state);
+	gameEnded.notifyAllObservers(pattern.size());
 }
--- memoryGameCore/memoryGameCore.h
+++ memoryGameCore/memoryGameCore.h
@@ -4,15 +4,26 @@ class MemoryGame {
 private:
 	std::vector<int> pattern = {};
 	int compareIndex = 0;
-	enum State { Starting, Displaying, Listening, Ending };
-	int gameState = State::Starting;
+	MemoryGame::State gameState = State::Starting;
 
+	/// <summary>
+	/// Appends an extra step to the existing pattern and returns as an integer array.
+	/// </summary>
+	/// <returns>The new pattern as an integer array.</returns>
 	int* getNextPattern();
+
+	/// <summary>
+	/// Processes an input and progresses the game.
+	/// </summary>
+	/// <param name="input">The input (between 0 and 3).</param>
 	void processNextInput(int input);
+
+	/// <summary>
+	/// Ends the game.
+	/// </summary>
 	void end();
 
-	template <typename T>
-	class GameEvent {
+	template <typename T> class GameEvent {
 	private:
 		std::vector<void (*)(T)> observers;
 	public:
@@ -21,9 +32,45 @@ private:
 		void notifyAllObservers(T data);
 	};
 
-	GameEvent<State> changedState;
+	GameEvent<MemoryGame::State> changedState;
 	GameEvent<int*> newPattern;
+	GameEvent<int> gameEnded;
 public:
+	/// <summary>
+	/// The current state of the game.
+	/// </summary>
+	enum class State { Starting, Active, Ending };
+
+	/// <summary>
+	/// The game events emitted during gameplay.
+	/// </summary>
+	enum class Event { onChangedState, onNewPattern, onGameEnded };
+
+	/// <summary>
+	/// Starts the game.
+	/// </summary>
 	void start();
-	void input(int value);
+
+	/// <summary>
+	/// Notifies the game of a user input.
+	/// </summary>
+	/// <param name="value">The input.</param>
+	/// <returns>True if the input is accepted.</returns>
+	bool input(int value);
+
+	/// <summary>
+	/// Attaches an observer to a specified game event.
+	/// </summary>
+	/// <param name="event">The event to attach the observer to.</param>
+	/// <param name="onEvent">The event handler.</param>
+	/// <returns>True if successful.</returns>
+	bool registerObserver(Event event, void (*onEvent)(auto));
+
+	/// <summary>
+	/// Detaches an observer from a specified game event.
+	/// </summary>
+	/// <param name="event">The event to detach the observer from.</param>
+	/// <param name="onEvent">The event handler.</param>
+	/// <returns>True if successful.</returns>
+	bool detachObserver(Event event, void (*onEvent)(auto));
 };
